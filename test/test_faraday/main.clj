fdfcb07364ed605f569e500867a2f82796f11dc6
Peter Taoussanis <ptaoussanis@gmail.com>
2013-05-27 21:14:47 +0700

    Clean up tests ns

--- test/test_faraday/main.clj
+++ test/test_faraday/main.clj
@@ -1,38 +1,37 @@
 (ns test-faraday.main
-  (:use     [clojure.test]
-            [taoensso.faraday])
-  (:require [taoensso.faraday :as far]) ; TODO
-  (:import [com.amazonaws.services.dynamodbv2.model
-            ConditionalCheckFailedException
-            ;; InternalServerErrorException
-            ;; ItemCollectionSizeLimitExceededException
-            ;; LimitExceededException
-            ;; ProvisionedThroughputExceededException
-            ;; ResourceInUseException
-            ResourceNotFoundException]))
+  (:use     [clojure.test])
+  (:require [taoensso.faraday :as far])
+  (:import  [com.amazonaws.services.dynamodbv2.model
+             ConditionalCheckFailedException
+             ;; InternalServerErrorException
+             ;; ItemCollectionSizeLimitExceededException
+             ;; LimitExceededException
+             ;; ProvisionedThroughputExceededException
+             ;; ResourceInUseException
+             ResourceNotFoundException]))
 
 ;; TODO LOTS of tests still outstanding (PRs welcome!!)
 
 (def creds {:access-key (get (System/getenv) "AWS_DYNAMODB_ACCESS_KEY")
             :secret-key (get (System/getenv) "AWS_DYNAMODB_SECRET_KEY")})
 
-(def table "rotary-dev-test")
+(def table "test-faraday.main")
 (def id    "test-id")
 (def attr  "test-attr")
 
 (defn setup-table!
   []
-  (ensure-table creds {:name       table
-                       :hash-key   {:name id :type :s}
-                       :throughput {:write 1 :read 1}})
+  (far/ensure-table creds {:name       table
+                           :hash-key   {:name id :type :s}
+                           :throughput {:read 1 :write 1}})
 
-  (batch-write-item creds
+  (far/batch-write-item creds
     [:delete table {id "1"}]
     [:delete table {id "2"}]
     [:delete table {id "3"}]
     [:delete table {id "4"}])
 
-  (batch-write-item creds
+  (far/batch-write-item creds
     [:put table {id "1" attr "foo"}]
     [:put table {id "2" attr "bar"}]
     [:put table {id "3" attr "baz"}]
@@ -42,27 +41,27 @@
   (setup-table!)
 
   (let [result
-        (batch-get-item creds
+        (far/batch-get-item creds
           {table {:key-name "test-id"
                   :keys ["1" "2" "3" "4"]
                   :consistent true}})
         consis
-        (batch-get-item creds
+        (far/batch-get-item creds
           {table {:key-name "test-id"
                   :keys ["1" "2" "3" "4"]
                   :consistent true}})
         attrs
-        (batch-get-item creds
+        (far/batch-get-item creds
           {table {:key-name "test-id"
                   :keys ["1" "2" "3" "4"]
                   :consistent true
                   :attrs [attr]}})
 
         items  (get-in result [:responses table])
-        item-1 (get-item creds table {id "1"})
-        item-2 (get-item creds table {id "2"})
-        item-3 (get-item creds table {id "3"})
-        item-4 (get-item creds table {id "4"})]
+        item-1 (far/get-item creds table {id "1"})
+        item-2 (far/get-item creds table {id "2"})
+        item-3 (far/get-item creds table {id "3"})
+        item-4 (far/get-item creds table {id "4"})]
 
     (is (= "foo"    (item-1 attr)) "batch-write-item :put failed")
     (is (= "bar"    (item-2 attr)) "batch-write-item :put failed")
@@ -74,42 +73,42 @@
     (is (= true (some #(= (% attr) "baz") (get-in attrs [:responses table]))))
     (is (= true (some #(= (% attr) "foobar") items)))
 
-    (batch-write-item creds
+    (far/batch-write-item creds
       [:delete table {id "1"}]
       [:delete table {id "2"}]
       [:delete table {id "3"}]
       [:delete table {id "4"}])
 
-    (is (= nil (get-item creds table {id "1"})) "batch-write-item :delete failed")
-    (is (= nil (get-item creds table {id "2"})) "batch-write-item :delete failed")
-    (is (= nil (get-item creds table {id "3"})) "batch-write-item :delete failed")
-    (is (= nil (get-item creds table {id "4"})) "batch-write-item :delete failed")))
+    (is (= nil (far/get-item creds table {id "1"})) "batch-write-item :delete failed")
+    (is (= nil (far/get-item creds table {id "2"})) "batch-write-item :delete failed")
+    (is (= nil (far/get-item creds table {id "3"})) "batch-write-item :delete failed")
+    (is (= nil (far/get-item creds table {id "4"})) "batch-write-item :delete failed")))
 
 (deftest conditional-put
-  (batch-write-item creds
+  (far/batch-write-item creds
    [:delete table {id "42"}]
    [:delete table {id "9"}]
    [:delete table {id "6"}]
    [:delete table {id "23"}])
 
-  (batch-write-item creds
+  (far/batch-write-item creds
    [:put table {id "42" attr "foo"}]
    [:put table {id "6"  attr "foobar"}]
    [:put table {id "9"  attr "foobaz"}])
 
   ;; Should update item 42 to have attr bar
-  (put-item creds table {id "42" attr "bar"} {:expected {attr "foo"}})
-  (is (= "bar" ((get-item creds table {id "42"}) attr)))
+  (far/put-item creds table {id "42" attr "bar"} {:expected {attr "foo"}})
+  (is (= "bar" ((far/get-item creds table {id "42"}) attr)))
 
   ;; Should fail to update item 6
   (is (thrown? ConditionalCheckFailedException
-               (put-item creds table {id "6" attr "baz"} {:expected {id false}})))
-  (is (not (= "baz" ((get-item creds table {id "6"}) attr))))
+               (far/put-item creds table {id "6" attr "baz"} {:expected {id false}})))
+  (is (not= "baz" ((far/get-item creds table {id "6"}) attr)))
 
   ;; Should upate item 9 to have attr baz
-  (put-item creds table {id "9" attr "baz"} {:expected {attr "foobaz"}})
-  (is (= "baz" ((get-item creds table {id "9"}) attr)))
+  (far/put-item creds table {id "9" attr "baz"} {:expected {attr "foobaz"}})
+  (is (= "baz" ((far/get-item creds table {id "9"}) attr)))
 
   ;; Should add item 23
-  (put-item creds table {id "23" attr "bar"} {:expected {id false}})
-  (is (not (= nil (get-item creds table {id "23"})))))
+  (far/put-item creds table {id "23" attr "bar"} {:expected {id false}})
+  (is (not= nil (far/get-item creds table {id "23"}))))
