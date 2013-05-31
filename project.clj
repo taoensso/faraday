72d6291dd5b9286801c15a5e17840011c4486b33
Peter Taoussanis <ptaoussanis@gmail.com>
2013-05-31 17:51:46 +0700

    Bump version (0.4.0)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/faraday "0.2.0"
+(defproject com.taoensso/faraday "0.4.0"
   :description "Clojure DynamoDB client"
   :url "https://github.com/ptaoussanis/faraday"
   :license {:name "Eclipse Public License"
