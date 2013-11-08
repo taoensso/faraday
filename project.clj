e61962f9350019c831a7ae1045b7a5aef0716eb1
Peter Taoussanis <ptaoussanis@gmail.com>
2013-11-08 21:41:04 +0700

    v1.0.0-RC2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/faraday "1.0.0-RC1"
+(defproject com.taoensso/faraday "1.0.0-RC2"
   :description "Clojure DynamoDB client"
   :url "https://github.com/ptaoussanis/faraday"
   :license {:name "Eclipse Public License"
