cdd981493d26b97b9bfe8e8934ba9f1307e82da2
Peter Taoussanis <ptaoussanis@gmail.com>
2013-11-18 14:20:14 +0700

    Deps: Nippy 2.5.0, Clojure 1.6.0-alpha2 (for testing), lein-ancient 0.5.4

--- project.clj
+++ project.clj
@@ -8,9 +8,9 @@
                  ;; TODO ; 1.5.x seems to cause problems with (at least)
                  ;; `create-table`: needs investigation
                  [com.amazonaws/aws-java-sdk "1.4.4.2"]
-                 [com.taoensso/nippy         "2.5.0-RC2"]]
+                 [com.taoensso/nippy         "2.5.0"]]
   :profiles {:1.5   {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6   {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :1.6   {:dependencies [[org.clojure/clojure "1.6.0-alpha2"]]}
              :dev   {:dependencies []}
              :test  {:dependencies [[expectations "1.4.56"]]}
              :bench {:dependencies [] :jvm-opts ["-server"]}}
@@ -21,7 +21,7 @@
             "codox"       ["with-profile" "+test" "doc"]}
   :plugins [[lein-expectations "0.0.8"]
             [lein-autoexpect   "1.0"]
-            [lein-ancient      "0.5.3"]
+            [lein-ancient      "0.5.4"]
             [codox             "0.6.6"]]
   :min-lein-version "2.0.0"
   :global-vars {*warn-on-reflection* true}
