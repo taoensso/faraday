dc5ff71b2fc4fe6569d5e0dd8622b96e8d2cf4a7
Peter Taoussanis <ptaoussanis@gmail.com>
2014-02-23 20:36:03 +0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,30 +1,46 @@
 (defproject com.taoensso/faraday "1.1.1"
+  :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Clojure DynamoDB client"
   :url "https://github.com/ptaoussanis/faraday"
   :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure        "1.5.1"]
-                 [org.clojure/tools.macro    "0.1.5"]
-                 ;; TODO ; 1.5.x seems to cause problems with (at least)
-                 ;; `create-table`: needs investigation
-                 [com.amazonaws/aws-java-sdk "1.7.1"]
-                 [com.taoensso/nippy         "2.5.1"]]
-  :profiles {:1.5   {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6   {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}
-             :dev   {:dependencies []}
-             :test  {:dependencies [[expectations "1.4.56"]]}
-             :bench {:dependencies [] :jvm-opts ["-server"]}}
-  :aliases {"test-all"    ["with-profile" "+test,+1.5:+test,+1.6" "expectations"]
-            "test-auto"   ["with-profile" "+test" "autoexpect"]
-            "start-dev"   ["with-profile" "+dev,+test" "repl" ":headless"]
-            "start-bench" ["trampoline" "start-dev"]
-            "codox"       ["with-profile" "+test" "doc"]}
-  :plugins [[lein-expectations "0.0.8"]
-            [lein-autoexpect   "1.2.1"]
-            [lein-ancient      "0.5.4"]
-            [codox             "0.6.7"]]
-  :min-lein-version "2.0.0"
-  :global-vars {*warn-on-reflection* true}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "Same as Clojure"}
+  :min-lein-version "2.3.3"
+  :global-vars {*warn-on-reflection* true
+                *assert* true}
+  :dependencies
+  [[org.clojure/clojure        "1.5.1"]
+   [com.amazonaws/aws-java-sdk "1.7.1"]
+   [com.taoensso/encore        "0.8.0"]
+   [com.taoensso/nippy         "2.5.2"]]
+
+  :test-paths ["test" "src"]
+  :profiles
+  {:build {:hooks ^:replace []} ; Workaround to avoid :dev hooks during deploy
+   :1.6  {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}
+   :test {:dependencies [[expectations            "1.4.56"]
+                         [reiddraper/simple-check "0.5.6"]]
+          :plugins [[lein-expectations "0.0.8"]
+                    [lein-autoexpect   "1.2.2"]]}
+   :dev
+   [:1.6 :test
+    {:jvm-opts ^:replace ["-server"]
+     :hooks []
+     :dependencies []
+     :plugins []}]}
+
+  :plugins [[lein-ancient "0.5.4"]
+            [codox        "0.6.7"]]
+
+  ;; :codox {:sources ["target/classes"]} ; For use with cljx
+  :aliases
+  {"test-all"   ["with-profile" "+test:+1.6,+test" "expectations"]
+   "test-auto"  ["with-profile" "+test" "autoexpect"]
+   "start-dev"  ["with-profile" "+dev" "repl" ":headless"]
+   "codox"      ["with-profile" "+test" "doc"]
+   "deploy-lib" ["with-profile" "+dev,+build" "do" "deploy" "clojars," "install"]}
+
   :repositories
   {"sonatype"
    {:url "http://oss.sonatype.org/content/repositories/releases"
