5d7b435db86d938967b4b9403a7b7df592d51869
Peter Taoussanis <ptaoussanis@gmail.com>
2014-07-10 18:08:09 +0700

    Update project.clj template

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
   :min-lein-version "2.3.3"
   :global-vars {*warn-on-reflection* true
                 *assert* true}
+
   :dependencies
   [[org.clojure/clojure        "1.5.1"]
    [com.taoensso/encore        "1.6.0"]
@@ -16,31 +17,27 @@
    [com.amazonaws/aws-java-sdk "1.7.8.1" :exclusions [joda-time]]
    [joda-time                  "2.3"]]
 
-  :test-paths ["test" "src"]
   :profiles
   {;; :default [:base :system :user :provided :dev]
+   :server-jvm {:jvm-opts ^:replace ["-server"]}
    :1.6  {:dependencies [[org.clojure/clojure    "1.6.0"]]}
    :test {:dependencies [[expectations           "1.4.56"]
                          [org.clojure/test.check "0.5.7"]]
           :plugins [[lein-expectations "0.0.8"]
                     [lein-autoexpect   "1.2.2"]]}
-   :dev* [:dev {:jvm-opts ^:replace ["-server"]
-                ;; :hooks [cljx.hooks leiningen.cljsbuild] ; cljx
-                }]
+
    :dev
    [:1.6 :test
-    {:dependencies []
-     :plugins [[lein-ancient "0.5.4"]
+    {:plugins [[lein-ancient "0.5.4"]
                [codox        "0.6.7"]]}]}
 
-  ;; :codox {:sources ["target/classes"]} ; cljx
+  :test-paths ["test" "src"]
+
   :aliases
   {"test-all"   ["with-profile" "default:+1.6" "expectations"]
    "test-auto"  ["with-profile" "+test" "autoexpect"]
-   ;; "build-once" ["do" "cljx" "once," "cljsbuild" "once"] ; cljx
-   ;; "deploy-lib" ["do" "build-once," "deploy" "clojars," "install"] ; cljx
    "deploy-lib" ["do" "deploy" "clojars," "install"]
-   "start-dev"  ["with-profile" "+dev*" "repl" ":headless"]}
+   "start-dev"  ["with-profile" "+server-jvm" "repl" ":headless"]}
 
   :repositories
   {"sonatype"
