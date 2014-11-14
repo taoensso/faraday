78ce553ee7fddc635db3a8c6d81339aba2dbe5f3
Peter Taoussanis <ptaoussanis@gmail.com>
2014-11-14 13:03:59 +0700

    Bump deps

--- project.clj
+++ project.clj
@@ -12,9 +12,9 @@
 
   :dependencies
   [[org.clojure/clojure        "1.5.1"]
-   [com.taoensso/encore        "1.14.0"]
+   [com.taoensso/encore        "1.15.1"]
    [com.taoensso/nippy         "2.7.0"]
-   [com.amazonaws/aws-java-sdk "1.9.3" :exclusions [joda-time]]
+   [com.amazonaws/aws-java-sdk "1.9.6" :exclusions [joda-time]]
    [joda-time                  "2.5"] ; For exclusion, see Github #27
    ]
 
@@ -23,7 +23,7 @@
    :server-jvm {:jvm-opts ^:replace ["-server"]}
    :1.7  {:dependencies [[org.clojure/clojure    "1.7.0-alpha3"]]}
    :1.6  {:dependencies [[org.clojure/clojure    "1.6.0"]]}
-   :test {:dependencies [[expectations           "2.0.12"]
+   :test {:dependencies [[expectations           "2.0.13"]
                          [org.clojure/test.check "0.5.9"]]
           :plugins [[lein-expectations "0.0.8"]
                     [lein-autoexpect   "1.4.0"]]}
