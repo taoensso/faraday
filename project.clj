a9c7788c12c00eb3dc76fa8eb97d676594147e2b
Peter Taoussanis <ptaoussanis@gmail.com>
2015-11-30 11:46:18 +0700

    Bump deps

--- project.clj
+++ project.clj
@@ -12,21 +12,23 @@
 
   :dependencies
   [[org.clojure/clojure "1.5.1"]
-   [com.taoensso/encore "2.18.0"]
-   [com.taoensso/nippy  "2.9.1"]
-   [joda-time           "2.8.2"] ; For exclusion, see Github #27
-   [com.amazonaws/aws-java-sdk-dynamodb "1.9.39"
+   [com.taoensso/encore "2.26.1"]
+   [com.taoensso/nippy  "2.10.0"]
+   [joda-time           "2.9.1"] ; For exclusion, see Github #27
+   [com.amazonaws/aws-java-sdk-dynamodb "1.9.39" ; TODO 1.10.x
     :exclusions [joda-time]]]
 
   :profiles
   {;; :default [:base :system :user :provided :dev]
    :server-jvm {:jvm-opts ^:replace ["-server"]}
-   :1.7  {:dependencies [[org.clojure/clojure    "1.7.0"]]}
+   :1.5  {:dependencies [[org.clojure/clojure    "1.5.1"]]}
    :1.6  {:dependencies [[org.clojure/clojure    "1.6.0"]]}
-   :test {:dependencies [[expectations           "2.1.0"]
-                         [org.clojure/test.check "0.8.2"]]
+   :1.7  {:dependencies [[org.clojure/clojure    "1.7.0"]]}
+   :1.8  {:dependencies [[org.clojure/clojure    "1.8.0-RC2"]]}
+   :test {:dependencies [[expectations           "2.1.4"]
+                         [org.clojure/test.check "0.9.0"]]
           :plugins [[lein-expectations "0.0.8"]
-                    [lein-autoexpect   "1.6.0"]]}
+                    [lein-autoexpect   "1.7.0"]]}
    :dev
    [:1.7 :test
     {:plugins [[lein-ancient "0.6.4"]
@@ -34,7 +36,7 @@
 
   :test-paths ["test" "src"]
   :aliases
-  {"test-all"   ["with-profile" "default:+1.6:+1.7" "expectations"]
+  {"test-all"   ["with-profile" "+1.5:+1.6:+1.7:+1.8" "expectations"]
    "test-auto"  ["with-profile" "+test" "autoexpect"]
    "deploy-lib" ["do" "deploy" "clojars," "install"]
    "start-dev"  ["with-profile" "+server-jvm" "repl" ":headless"]}
