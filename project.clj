f9275f25b2387415423717a1dd015ed79d91925b
Peter Taoussanis <ptaoussanis@gmail.com>
2016-10-01 15:49:37 +0700

    [#100] Revert to older Expectations dep to restore Clojure 1.5 support
    Would like to move away from Expectations in future, but not urgent.

--- project.clj
+++ project.clj
@@ -26,7 +26,7 @@
    :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9  {:dependencies [[org.clojure/clojure "1.9.0-alpha10"]]}
-   :test {:dependencies [[expectations           "2.1.9"]
+   :test {:dependencies [[expectations           "2.1.3"]
                          [org.clojure/test.check "0.9.0"]]
           :plugins [[lein-expectations "0.0.8"]
                     [lein-autoexpect   "1.9.0"]]}
