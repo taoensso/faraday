74558a343cb109b5dad579f3b7806ef338cf8f77
Joe Littlejohn <joelittlejohn@gmail.com>
2020-07-18 18:02:42 +0100

    Prepare for 1.11.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/faraday "1.11.0-alpha2-SNAPSHOT"
+(defproject com.taoensso/faraday "1.11.0-SNAPSHOT"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Clojure DynamoDB client"
   :url "https://github.com/ptaoussanis/faraday"
