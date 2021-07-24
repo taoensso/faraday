76dced4f99c025fbeab639a7989cdadcecea3701
Joe Littlejohn <joe@juxt.pro>
2021-07-24 19:20:52 +0100

    Version 1.11.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/faraday "1.11.2"
+(defproject com.taoensso/faraday "1.11.3-SNAPSHOT"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Clojure DynamoDB client"
   :url "https://github.com/ptaoussanis/faraday"
