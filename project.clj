6f813f7d8649fb1734134cfd036023479fb23df0
Peter Taoussanis <peter@taoensso.com>
2023-07-25 17:38:12 +0200

    Update project template
    Sync project boilerplate used across Taoensso libraries to help keep
    things easier to maintain, and to prepare for some upcoming GraalVM
    tests.
    
    Note:
      - Adds tests under Clojure 1.11
      - Removes tests under Clojure 1.7, 1.8 which are both now 7+
        years old.

--- project.clj
+++ project.clj
@@ -2,17 +2,13 @@
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Amazon DynamoDB client for Clojure"
   :url "https://github.com/ptaoussanis/faraday"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"
-            :distribution :repo
-            :comments "Same as Clojure"}
-  :min-lein-version "2.3.3"
-  :global-vars {*warn-on-reflection* true
-                *assert* true}
+
+  :license
+  {:name "Eclipse Public License - v 1.0"
+   :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies
-  [[org.clojure/clojure "1.7.0"]
-   [com.taoensso/encore "3.34.0" :exclusions [org.clojure/tools.reader]]
+  [[com.taoensso/encore "3.34.0" :exclusions [org.clojure/tools.reader]]
    [com.taoensso/nippy  "3.2.0"]
    [joda-time           "2.12.2"]
    [commons-logging     "1.2"]
@@ -20,34 +16,56 @@
     :exclusions [joda-time commons-logging]]]
 
   :profiles
-  {:server-jvm {:jvm-opts ^:replace ["-server"]}
-   :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
-   :dev
-   [:1.10 :server-jvm
-    {:dependencies [[org.testcontainers/testcontainers "1.17.6" :exclusions [com.fasterxml.jackson.core/jackson-annotations]]
-                    [org.slf4j/slf4j-simple "1.7.36"]]
-     :plugins [[lein-ancient "0.7.0"]
-               [lein-codox   "0.10.8"]]}]}
-  :test-paths ["test" "src"]
+  {;; :default [:base :system :user :provided :dev]
+   :provided {:dependencies [[org.clojure/clojure "1.11.1"]]}
+   :c1.11    {:dependencies [[org.clojure/clojure "1.11.1"]]}
+   :c1.10    {:dependencies [[org.clojure/clojure "1.10.3"]]}
+   :c1.9     {:dependencies [[org.clojure/clojure "1.9.0"]]}
 
-  :codox
-  {:language :clojure
-   :source-uri "https://github.com/ptaoussanis/faraday/blob/master/{filepath}#L{line}"}
+   :test
+   {:jvm-opts ["-Dtaoensso.elide-deprecated=true"]
+    :global-vars
+    {*warn-on-reflection* true
+     *assert*             true
+     *unchecked-math*     false #_:warn-on-boxed}}
+
+   :dev
+   [:c1.11 :test
+    {:jvm-opts ["-server"]
+     :dependencies
+     [[org.testcontainers/testcontainers "1.17.6"
+       :exclusions [com.fasterxml.jackson.core/jackson-annotations]]
+      [org.slf4j/slf4j-simple "1.7.36"]]
+
+     :plugins
+     [[lein-pprint  "1.3.2"]
+      [lein-ancient "0.7.0"]
+      [com.taoensso.forks/lein-codox "0.10.10"]]
+
+     :codox
+     {:language #{:clojure #_:clojurescript}
+      :base-language :clojure}}]}
+
+  :test-paths ["test" #_"src"]
 
   :aliases
-  {"test-all" ["with-profile" "+1.10:+1.9:+1.8:+1.7" "test"]}
+  {"start-dev"     ["with-profile" "+dev" "repl" ":headless"]
+   ;; "build-once" ["do" ["clean"] ["cljsbuild" "once"]]
+   "deploy-lib"    ["do" #_["build-once"] ["deploy" "clojars"] ["install"]]
 
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag" "v"]
-                  ["with-profile" "-dev" "deploy" "clojars"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]
-                  ["vcs" "push"]]
+   "test-clj"     ["with-profile" "+c1.11:+c1.10:+c1.9" "test"]
+   ;; "test-cljs" ["with-profile" "+test" "cljsbuild"   "test"]
+   "test-all"     ["do" ["clean"] ["test-clj"] #_["test-cljs"]]}
+
+  :release-tasks
+  [["vcs" "assert-committed"]
+   ["change" "version" "leiningen.release/bump-version" "release"]
+   ["vcs" "commit"]
+   ["vcs" "tag" "v"]
+   ["with-profile" "-dev" "deploy" "clojars"]
+   ["change" "version" "leiningen.release/bump-version"]
+   ["vcs" "commit"]
+   ["vcs" "push"]]
 
   :repositories {"releases" {:url "https://clojars.org/repo"
                              :creds :gpg}})
