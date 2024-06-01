3bc0ad6e7247a4b9cfdb28636d93131e111a3dc9
Joe Littlejohn <joelittlejohn@gmail.com>
2024-06-01 19:10:00 +0100

    Version 1.12.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/faraday "1.12.3"
+(defproject com.taoensso/faraday "1.12.4-SNAPSHOT"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Amazon DynamoDB client for Clojure"
   :url "https://www.taoensso.com/faraday"
